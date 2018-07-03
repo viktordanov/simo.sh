180197227afd82387de664f339ae3f12a5a58670
Simo Aleksandrov <simo3003@me.com>
2018-07-03 21:21:27 +0300

    Replace jQuery lib from slim to full, Add slideToggles to divs, Write Project div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
-// $(document).ready(function () {
+ $(document).ready(function () {
+    $('#aboutDiv').hide();
+    $('#projectsDiv').hide();
+    $('#linksDiv').hide();
 
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+    $('#aboutA').click(function() {
+        $('#aboutDiv').slideToggle();
+    });
+
+    $('#projectsA').click(function() {
+        $('#projectsDiv').slideToggle();
+    });
+
+    $('#linksA').click(function() {
+        $('#linksDiv').slideToggle();
+    });
 // $('#goToLinks').click(function({
 // $('html, body').animate({
 // scrollTop: $("#links").offset().top
 // }, 500); 
 // }));
-// });
+ });
