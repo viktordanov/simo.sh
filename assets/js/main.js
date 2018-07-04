76eb742b0f8a4380dd1de49112e0ccac45cff1d8
Simo Aleksandrov <simo3003@me.com>
2018-07-04 03:07:31 +0300

    Add anchors and animate them

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,42 @@
- $(document).ready(function () {
+$(document).ready(function () {
     $('#aboutDiv').hide();
-    // $('#projectsDiv').hide();
+    $('#projectsDiv').hide();
     $('#linksDiv').hide();
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
-    $('#aboutA').click(function() {
+    $('#aboutA').click(function () {
         $('#aboutDiv').slideToggle();
     });
 
-    $('#projectsA').click(function() {
+    $('#projectsA').click(function () {
         $('#projectsDiv').slideToggle();
     });
 
-    $('#linksA').click(function() {
+    $('#linksA').click(function () {
         $('#linksDiv').slideToggle();
     });
-// $('#goToLinks').click(function({
-// $('html, body').animate({
-// scrollTop: $("#links").offset().top
-// }, 500); 
-// }));
- });
+
+    $('#aboutButton').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#aboutAnchor").offset().top
+        }, 400)
+        $('#aboutDiv').slideDown(800);
+    });
+
+    $('#projectsButton').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#projectsAnchor").offset().top
+        }, 400)
+        $('#projectsDiv').slideDown(800);
+    });
+
+    $('linksButton').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#linksAnchor").offset().top
+        }, 400)
+        $('#linkDiv').slideDown(800);
+    });
+
+});
