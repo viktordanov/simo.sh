5069365d1262565f52abb83b327e422a51d4dbd9
Simo Aleksandrov <simo3003@me.com>
2019-08-12 11:14:03 +0200

    plugin

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   mozjpeg: { progressive: true, quality: 75 },
-  pngquant: { quality: 75 },
   svgo: {
     plugins: [{ removeViewBox: false }, { cleanupIDs: true }]
   },
   webp: { quality: 75 }
-};
+}
