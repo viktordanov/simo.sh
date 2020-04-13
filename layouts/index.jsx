33e8c751a1d8f5cc8e8719a67ed60728bc50cbe1
Simo Aleksandrov <simo3003@me.com>
2020-04-13 23:20:17 +0300

    btn on achievemtns

--- layouts/index.jsx
+++ layouts/index.jsx
@@ -1,13 +1,29 @@
-import { Box, Heading } from "@chakra-ui/core"
+import { Badge, Box, Flex, Heading } from "@chakra-ui/core"
 import { Main } from "../src/components/Main"
 
 export default (frontMatter) => {
+  console.log(frontMatter)
+
   return ({ children }) => {
     return (
       <>
         <Heading fontSize="3.6rem" mb="1rem" textAlign="center">
           {frontMatter.title}
         </Heading>
+        <Flex wrap="wrap">
+          {frontMatter.tags?.map((t) => (
+            <Badge
+              rounded="full"
+              variantColor="teal"
+              key={t}
+              px="0.4rem"
+              mr={1}
+              my={[1, 0]}
+            >
+              {t}
+            </Badge>
+          ))}
+        </Flex>
         <Main>
           <Box py="3rem" px="2rem" bgColor="white">
             {children}
--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -1,8 +1,9 @@
-import { Badge, Box, Flex, Image, Text } from "@chakra-ui/core"
+import { Badge, Box, Flex, IconButton, Image, Text } from "@chakra-ui/core"
+import NextLink from "next/link"
 
-export const Achievement = ({ name, images, tags }) => (
-  <Box maxW="sm" borderWidth="1px" rounded={10} overflow="hidden" h="100%">
-    <Image src={images[0]} alt={name} objectFit="cover" />
+export const Achievement = ({ name, link, images, tags }) => (
+  <Box borderWidth="1px" rounded={10} overflow="hidden" h="100%">
+    <Image src={images[0]} alt={name} objectFit="contain" />
     <Box p="6">
       <Flex wrap="wrap">
         {tags.map((t) => (
@@ -11,16 +12,31 @@ export const Achievement = ({ name, images, tags }) => (
           </Badge>
         ))}
       </Flex>
-      <Text
-        mt="1"
-        ml="0.3rem"
-        fontWeight="semibold"
-        fontSize="2xl"
-        lineHeight="tight"
-        isTruncated
-      >
-        {name}
-      </Text>
+      <Flex justifyContent="space-between">
+        <Text
+          mt="1"
+          ml="0.3rem"
+          fontWeight="semibold"
+          fontSize="2xl"
+          lineHeight="tight"
+          isTruncated
+        >
+          {name}
+        </Text>
+        <Flex justifyContent="center">
+          {link != "" && (
+            <NextLink href={link}>
+              <IconButton
+                ml={1}
+                icon="chevron-right"
+                size="lg"
+                variant="ghost"
+                aria-label="Go to project"
+              />
+            </NextLink>
+          )}
+        </Flex>
+      </Flex>
     </Box>
   </Box>
 )
