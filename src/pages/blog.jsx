a289f707291c23f91966807dbd289b8bcdacda21
Simo Aleksandrov <simo3003@me.com>
2020-04-14 21:44:36 +0300

    blog page

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,2 +1,6 @@
 import { Layout } from "../layouts/index"
-export default () => <Layout title="Blog ✏">hi</Layout>
+export default () => (
+  <Layout title="Blog ✏">
+    This is going to be my blog, as of now there are 0 posts.
+  </Layout>
+)
