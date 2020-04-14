fd9502f7d1e38d77ab8b76aea0f944be0ab4935e
Simo Aleksandrov <simo3003@me.com>
2020-04-14 13:27:42 +0300

    better colors

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -1,42 +1,61 @@
-import { Badge, Box, Flex, IconButton, Image, Text } from "@chakra-ui/core"
+import {
+  Badge,
+  Box,
+  Flex,
+  IconButton,
+  Image,
+  Text,
+  useColorMode,
+} from "@chakra-ui/core"
 import NextLink from "next/link"
 
-export const Achievement = ({ title, slug = "", image, tags }) => (
-  <Box borderWidth="1px" rounded={10} overflow="hidden" h="100%">
-    <Image src={image} alt={title} objectFit="contain" />
-    <Box p="6">
-      <Flex wrap="wrap">
-        {tags.map((t) => (
-          <Badge rounded="full" px={2} variantColor="teal" key={t} m="0.3rem">
-            {t}
-          </Badge>
-        ))}
-      </Flex>
-      <Flex justifyContent="space-between">
-        <Text
-          mt="1"
-          ml="0.3rem"
-          fontWeight="semibold"
-          fontSize="2xl"
-          lineHeight="tight"
-          isTruncated
-        >
-          {title}
-        </Text>
-        <Flex justifyContent="center">
-          {slug != "" && (
-            <NextLink href={`achievements/${slug}`}>
-              <IconButton
-                ml={1}
-                icon="chevron-right"
-                size="lg"
-                variant="ghost"
-                aria-label="Go to project"
-              />
-            </NextLink>
-          )}
+export const Achievement = ({ title, slug = "", image, tags }) => {
+  const { colorMode } = useColorMode()
+  const bgColor = { light: "gray.50", dark: "gray.900" }
+
+  return (
+    <Box
+      rounded={10}
+      backgroundColor={bgColor[colorMode]}
+      overflow="hidden"
+      h="100%"
+      boxShadow="0px 10px 50px rgba(0, 0, 0, 0.3)"
+    >
+      <Image src={image} alt={title} objectFit="contain" />
+      <Box p="6">
+        <Flex wrap="wrap">
+          {tags.map((t) => (
+            <Badge rounded="full" px={2} variantColor="teal" key={t} m="0.3rem">
+              {t}
+            </Badge>
+          ))}
         </Flex>
-      </Flex>
+        <Flex justifyContent="space-between">
+          <Text
+            mt="1"
+            ml="0.3rem"
+            fontWeight="semibold"
+            fontSize="2xl"
+            lineHeight="tight"
+            isTruncated
+          >
+            {title}
+          </Text>
+          <Flex justifyContent="center">
+            {slug != "" && (
+              <NextLink href={`achievements/${slug}`}>
+                <IconButton
+                  ml={1}
+                  icon="chevron-right"
+                  size="lg"
+                  variant="ghost"
+                  aria-label="Go to project"
+                />
+              </NextLink>
+            )}
+          </Flex>
+        </Flex>
+      </Box>
     </Box>
-  </Box>
-)
+  )
+}
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,49 +1,55 @@
-import { Flex, Heading, IconButton, Image } from "@chakra-ui/core"
+import { Flex, Heading, IconButton, Image, useColorMode } from "@chakra-ui/core"
 import { css } from "@emotion/core"
 import NextLink from "next/link"
-export const ProjectCard = ({ title, slug, image, logo }) => (
-  <Flex
-    direction="column"
-    boxShadow="0px 10px 50px rgba(0, 0, 0, 0.3)"
-    rounded={15}
-    p={5}
-  >
-    <Image
-      opacity={1}
-      src={image}
+export const ProjectCard = ({ title, slug, image, logo }) => {
+  const { colorMode } = useColorMode()
+  const bgColor = { light: "gray.50", dark: "gray.900" }
+
+  return (
+    <Flex
+      direction="column"
+      boxShadow="0px 10px 50px rgba(0, 0, 0, 0.3)"
+      backgroundColor={bgColor[colorMode]}
       rounded={15}
-      objectFit="contain"
-      objectPosition="50% 100%"
-    />
-    <Flex justifyContent="center" alignItems="center" className="logo-flex">
+      p={5}
+    >
       <Image
-        src={logo}
+        opacity={1}
+        src={image}
+        rounded={15}
         objectFit="contain"
-        size={85}
-        className="logo"
-        mt={-12}
-        css={css`
-          &:hover {
-            transform: scale(1.1);
-          }
-        `}
-        transition="all 0.25s ease-in-out"
+        objectPosition="50% 100%"
       />
+      <Flex justifyContent="center" alignItems="center" className="logo-flex">
+        <Image
+          src={logo}
+          objectFit="contain"
+          size={85}
+          className="logo"
+          mt={-12}
+          css={css`
+            &:hover {
+              transform: scale(1.1);
+            }
+          `}
+          transition="all 0.25s ease-in-out"
+        />
+      </Flex>
+      <Heading mt={2} textAlign="center">
+        {title}
+      </Heading>
+      <Flex justifyContent="center">
+        {slug != "" && (
+          <NextLink href={`projects/${slug}`}>
+            <IconButton
+              icon="info-outline"
+              size="lg"
+              variant="ghost"
+              aria-label="Go to project"
+            />
+          </NextLink>
+        )}
+      </Flex>
     </Flex>
-    <Heading mt={2} textAlign="center">
-      {title}
-    </Heading>
-    <Flex justifyContent="center">
-      {slug != "" && (
-        <NextLink href={`projects/${slug}`}>
-          <IconButton
-            icon="info-outline"
-            size="lg"
-            variant="ghost"
-            aria-label="Go to project"
-          />
-        </NextLink>
-      )}
-    </Flex>
-  </Flex>
-)
+  )
+}
