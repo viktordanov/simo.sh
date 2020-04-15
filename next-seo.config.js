a15c5f90a50f57e8d33f0f84807393ef72f44b5d
Simo Aleksandrov <simo3003@me.com>
2020-04-15 03:28:29 +0300

    fix seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -13,7 +13,7 @@ const SEO = {
     description,
     images: [
       {
-        url: "https://simo.sh/dp-scrot.png",
+        url: "https://simo.sh/dp-scrot.jpg",
         alt: title,
         width: 1280,
         height: 720,
