417a0ee5cfb9e6eea6f17a7fb176cb4847309e47
Simo Aleksandrov <simo3003@me.com>
2020-08-22 21:50:54 +0300

    Make Post component

---
+++ components/Post.jsx
@@ -0,0 +1,54 @@
+import { Badge, Box, Flex, PseudoBox, Text } from "@chakra-ui/core"
+
+export const Post = ({ title, slug, baseDir, tags, short }) => {
+  return (
+    <PseudoBox
+      transition="all 0.25s ease-in-out"
+      p={4}
+      rounded={15}
+      _hover={{
+        boxShadow: "0px 10px 50px rgba(0, 0, 0, 0.25)",
+      }}
+      boxShadow="0px 8px 30px rgba(0, 0, 0, 0.1)"
+    >
+      <Flex alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center">
+          <Box>
+            <Text fontSize="2xl" fontWeight="bold" mb={1}>
+              {title}
+            </Text>
+            <Flex wrap="wrap">
+              {tags?.length > 0 &&
+                tags.map((t) => (
+                  <Badge
+                    rounded="full"
+                    variantColor="red"
+                    key={t}
+                    px="0.4rem"
+                    mr={1}
+                    my={[1, 0]}
+                  >
+                    {t}
+                  </Badge>
+                ))}
+            </Flex>
+            {short && (
+              <Text mt={1} fontSize="xl">
+                {short}
+              </Text>
+            )}
+          </Box>
+        </Flex>
+        <NextLink href={`${baseDir}/${slug}`}>
+          <IconButton
+            justifySelf="end"
+            icon="chevron-right"
+            size="lg"
+            variant="ghost"
+            aria-label="Go to Post"
+          />
+        </NextLink>
+      </Flex>
+    </PseudoBox>
+  )
+}
