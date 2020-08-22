66272ab29bb0468fa747c3547262d8a99b535037
Simo Aleksandrov <simo3003@me.com>
2020-08-22 21:51:56 +0300

    Aggregate all posts in /posts

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,61 +1,42 @@
 import { Box, Heading } from "@chakra-ui/core"
 import { Main } from "../components/Main"
+import { Post } from "../components/Post"
 import { SEO } from "../components/SEO"
 import { Layout } from "../layouts/index"
 import { frontMatter as achievements } from "./achievements/**/*.mdx"
-import { frontMatter as randomPosts } from "./blog/**/*.mdx"
 import { frontMatter as projects } from "./projects/**/*.mdx"
+import { frontMatter as randomPosts } from "./random/**/*.mdx"
 
-const showcaserRandomPosts = randomPosts
-  .map((v) => {
-    if (v.order > 0) {
-      return v
-    }
+const generatePosts = (baseDir, arr) =>
+  arr
+    .map((v) => {
+      if (v.order > 0) {
+        return v
+      }
 
-    return { ...v, order: -1 }
-  })
-  .sort((a, b) => {
-    if (a.order > 0 && b.order > 0) {
-      return a.order - b.order
-    }
+      return { ...v, order: -1 }
+    })
+    .sort((a, b) => {
+      if (a.order > 0 && b.order > 0) {
+        return a.order - b.order
+      }
 
-    return b.order - a.order
-  })
-  .map((p) => <Box key={p.title}>{p.title}</Box>)
+      return b.order - a.order
+    })
+    .map((p) => (
+      <Post
+        key={p.title}
+        short={p.short}
+        tags={p.tags}
+        title={p.title}
+        slug={slug}
+        basedir={baseDir}
+      />
+    ))
 
-const showcaseProjects = projects
-  .map((v) => {
-    if (v.order > 0) {
-      return v
-    }
-
-    return { ...v, order: -1 }
-  })
-  .sort((a, b) => {
-    if (a.order > 0 && b.order > 0) {
-      return a.order - b.order
-    }
-
-    return b.order - a.order
-  })
-  .map((p) => <Box key={p.title}>{p.title}</Box>)
-
-const showcaseAchievements = achievements
-  .map((v) => {
-    if (v.order > 0) {
-      return v
-    }
-
-    return { ...v, order: -1 }
-  })
-  .sort((a, b) => {
-    if (a.order > 0 && b.order > 0) {
-      return a.order - b.order
-    }
-
-    return b.order - a.order
-  })
-  .map((a) => <Box key={a.title}>{a.title}</Box>)
+const showcaseRandomPosts = generatePosts("random", randomPosts)
+const showcaseProjects = generatePosts("projects", projects)
+const showcaseAchievements = genereatePosts("achievements", achievements)
 
 const Posts = () => (
   <>
@@ -68,6 +49,7 @@ const Posts = () => (
       <Main py="3rem">
         <Box>
           <Heading>Random</Heading>
+          {showcaseRandomPosts}
         </Box>
         <Box>
           <Heading>Achievements</Heading>
